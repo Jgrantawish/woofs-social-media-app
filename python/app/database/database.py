d448f7138385b912f45063cab2cd81c32f5509b5
Jgrantawish <joannaindiagrant@gmail.com>
2026-06-09 14:49:03 +0100

    restructure database related files into database and SQLModel models directories

---
+++ python/app/database/database.py
@@ -0,0 +1,19 @@
+from sqlmodel import create_engine, Session
+from ..models.models import SQLModel
+
+
+# Setup the Database Connection
+sqlite_url = "sqlite:///woofs.db"
+engine = create_engine(sqlite_url, echo=True) # echo=True shows the raw SQL in your console!
+
+# Create all of the above tables
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+create_db_and_tables()
+
+# Create all of the above tables
+def get_session():
+    with Session(engine) as session:
+        yield session
+
