53ccbe7e363a7b725770f190afb604d93ee4c91c
Jgrantawish <joannaindiagrant@gmail.com>
2026-06-09 14:58:35 +0100

    Add CORSMiddleware to allow Angular to connect to FastAPI

--- python/app/main.py
+++ python/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import auth, home
 
 app = FastAPI()
@@ -6,3 +7,10 @@ app = FastAPI()
 app.include_router(auth.router)
 #app.include_router(home.router)
 
+app.add_middleware(
+    CORSMiddleware, 
+    allow_origins=["http://localhost:4200"],
+    allow_credentials = True, 
+    allow_methods = ["*"],
+    allow_headers=["*"],
+)
