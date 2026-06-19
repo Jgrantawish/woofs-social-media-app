7eafd9b6d0e4204bd5f0db0375c592ad9bdedfa2
Jgrantawish <joannaindiagrant@gmail.com>
2026-06-19 13:15:57 +0100

    update hashing functions to check passwords against stored hashes

--- python/app/core/security.py
+++ python/app/core/security.py
@@ -35,8 +35,12 @@ def create_user_session(user_id: int, username: str):
 
 
 # Use the most modern hashing algorith to hash the user's password 
-hashing_algorith = PasswordHash.recommended()
+hashing_algorithm = PasswordHash.recommended()
 
 # Return the hash of the password passed in
 def hash_password(password: str):
-    return hashing_algorith.hash(password)
+    return hashing_algorithm.hash(password)
+
+# Verify the password against stored hash
+def verify_password(password: str, password_hash: str) -> bool:
+    return hashing_algorithm.verify(password, password_hash)
