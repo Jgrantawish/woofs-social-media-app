2de0be63f1b64726c5d06969251d01df8a529aa9
Jgrantawish <joannaindiagrant@gmail.com>
2026-07-03 18:24:21 +0100

    Create Response SQL Models for sending data to the front end

---
+++ python/app/models/responses.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from sqlmodel import SQLModel
+from typing import Optional
+
+# Create Models which outline the structure of Objects that will be sent to the front end 
+
+class CommentResponse (SQLModel):
+    id: int
+    content: str
+    created_date: datetime
+    last_updated: Optional[datetime] = None
+
+    owner_username: str
+    owner_profile_pic_url: Optional[str] = None
+
+class PostResponse (SQLModel):
+    id: int
+    picture_url: Optional[str] = None
+    content: Optional[str] = None
+    created_date: datetime
+    last_updated: Optional[datetime] = None
+
+    owner_username: str
+    owner_profile_pic_url: Optional[str] = None
+
+    like_count: int
+    has_liked: bool
+
+    comments: list[CommentResponse]
